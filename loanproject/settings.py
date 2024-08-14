1b339bc788a26af3ea283a4e88b4ed205e249331
quirinus <quirinusboy@gmail.com>
2024-08-14 13:37:19 +0700

    Add static

--- loanproject/settings.py
+++ loanproject/settings.py
@@ -95,7 +95,6 @@ WSGI_APPLICATION = 'loanproject.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
-import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
@@ -137,15 +136,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
